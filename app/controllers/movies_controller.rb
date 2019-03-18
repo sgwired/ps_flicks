44a384909d708c2a25ac4be258d480be6dc10c63
Shelton Gayles <sgbythelake@gmail.com>
2019-03-18 14:17:49 -0700

    added crud

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,36 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+  
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
+  end
+
+  def destroy 
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_url
+  end
+
+  private
+  def movie_params
+    movie_params = params.require(:movie).
+      permit(:title, :description, :released_on, :total_gross)
+  end
 end
